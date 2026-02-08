34aa282326262687b4971a9efc3085c1dcd06796
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-02-08 03:48:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Zyra": "Sistema Limpio Online"}
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok",
+        "timestamp": datetime.utcnow().isoformat()
+    }
