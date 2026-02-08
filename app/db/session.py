789ca403a6cd9b3d1509020dd0c2a64902319a45
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-02-08 10:38:25 -0800

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -5,10 +5,16 @@ from .base import Base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
