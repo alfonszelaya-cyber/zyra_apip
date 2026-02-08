d85d36b36fcde8dbe2b223c813b35b057ba85fc6
alfonszelaya-cyber <alfonszelaya@gmail.com>
2026-02-08 04:53:44 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes.health import router as health_router
+from app.routes.status import router as status_router
 
 app = FastAPI(
     title="ZYRA API",
@@ -10,5 +11,8 @@ app = FastAPI(
 def read_root():
     return {"Zyra": "Sistema Limpio Online"}
 
-# Inyecta la ruta health desde app/routes
+# Health route
 app.include_router(health_router)
+
+# Status route
+app.include_router(status_router)
